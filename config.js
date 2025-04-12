1e71427ace25432ca6c584d1e0fbf4bdf8e81597
buruhgambar <mahendrayusuf110@gmail.com>
2025-04-12 23:31:48 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,30 @@
+// config.js
+const config = {
+  companyName: "Nama Perusahaan Anda",
+  slogan: "Slogan keren di sini",
+  heroTitle: "Selamat Datang di Perusahaan Kami",
+  heroSubtitle: "Solusi terbaik untuk kebutuhan Anda",
+  about: "Deskripsi lengkap tentang perusahaan Anda di sini...",
+  services: [
+    { title: "Layanan 1", description: "Deskripsi layanan 1" },
+    { title: "Layanan 2", description: "Deskripsi layanan 2" },
+    { title: "Layanan 3", description: "Deskripsi layanan 3" },
+  ],
+  gallery: [
+    "/images/img1.jpg",
+    "/images/img2.jpg",
+    "/images/img3.jpg",
+    "/images/img4.jpg",
+    "/images/img5.jpg",
+    "/images/img6.jpg",
+  ],
+  contact: {
+    whatsapp: "6281234567890",
+    instagram: "nama_ig_anda",
+    facebook: "nama_fb_anda",
+    x: "nama_x_anda",
+  },
+  mapEmbed: "https://www.google.com/maps/embed?pb=...", // link embed Google Maps
+};
+
+export default config;
